7d9ba2f0f8ca944b18f0ca5932d9bad5f0839417
ChristianSchwer <christianschwer00@gmail.com>
2021-11-23 22:28:21 +0100

    Footer added map and message box

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -3,16 +3,33 @@ import { Link } from 'react-router-dom'
 import Logo from '../images/logo.png';
 import './Footer.css'
 
-function footer() {
+function Footer() {
   return (
     <div className='footer-container'>
+      <section className="footer-subscription-head">
+        <p className='footer-subscription-heading'>Kontaktiere uns oder schreibe eine Nachricht:</p>
+      </section>
       <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>Kontaktiere uns:</p>
         <p className='footer-subscription-text'>Modellbahn Kaufmann</p>
         <p className='footer-subscription-text'>Bahnhofstraße 12</p>
         <p className='footer-subscription-text'>6850 Dornbirn</p>
         <p className='footer-subscription-text'>+43 5572/54149</p>
         <p className='footer-subscription-text'>modellbahn@avidonet.at</p>
+      </section>
+      <section className="footer-message">
+        <input type="email" placeholder="Deine E-Mail Adresse"></input>
+        <textarea type="textarea" placeholder="Deine Nachricht" className="footer-message-text"></textarea>
+      </section>
+      <section className="footer-map">
+      <iframe 
+        title="map"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2699.7774189958686!2d9.736988215294573!3d47.41628250909938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479b6b4cd62aa1cd%3A0x43293b498cc161ea!2sModellbahn%20Kaufmann!5e0!3m2!1sde!2sat!4v1637700134436!5m2!1sde!2sat" 
+        width="100%" 
+        height="300px" 
+        style={{ border: "0" }}
+        allowFullScreen="" 
+        loading="lazy">
+      </iframe>
       </section>
       <section className='footer-links'>
         <div className='footer-logo'>
@@ -49,5 +66,5 @@ function footer() {
   )
 }
 
-export default footer
+export default Footer;
 
